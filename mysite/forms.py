d21276ba2817fc632f0e5becc70a2bf829ed229b
alstjr0307 <alswp07@naver.com>
2021-02-25 20:16:19 +0900

    ㅈㄴ바꿈

--- mysite/forms.py
+++ mysite/forms.py
@@ -3,18 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class UserCreationForm(UserCreationForm):
-<<<<<<< HEAD
     
-=======
-    email = forms.EmailField(label='이메일',required=True)
-    nickname = forms.CharField(label= '닉네임',max_length=12, min_length=2, required=True, help_text='Required: Nickname',
-                               widget=(forms.TextInput(attrs={'class': 'form-control'})))
-    labels = {
-        'username': '닉네임',
-        'email': '이메일',
-        'password': '패스워드'
-    }
->>>>>>> f64f7a8d0b9819a4a2822b323c99e833a7085ee2
 
     email = forms.EmailField(label='이메일',required=True, widget=(forms.TextInput(attrs={'class': 'form-control'})))
     nickname = forms.CharField(label= '닉네임',max_length=6, min_length=2, required=True,
@@ -26,13 +15,9 @@ class UserCreationForm(UserCreationForm):
         labels = {
         'username': '아이디',
         'email': '이메일',
-        'password1': '패스워드',
-        'password2': '패스워드 확인',
         }
         widgets = {
         'username': forms.TextInput(attrs={'class': 'form-control', 'placeholder':'15자 이내로 입력 가능합니다.'}),
-        'password1' : forms.PasswordInput(attrs={'class': 'form-control'}),
-        'password2' : forms.PasswordInput(attrs={'class': 'form-control'}),
         }
 
 
--- mysite/tokens.py
+++ mysite/tokens.py
@@ -1,5 +1,4 @@
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
-<<<<<<< HEAD
 
 import six
 
@@ -7,14 +6,6 @@ class AccountActivationTokenGenerator(PasswordResetTokenGenerator):
     def _make_hash_value(self, user, timestamp):
         return (
             six.text_type(user.pk) + six.text_type(timestamp) + six.text_type(user.is_active)
-=======
-import six
-class AccountActivationTokenGenerator(PasswordResetTokenGenerator):
-    def _make_hash_value(self, user, timestamp):
-        return (
-            six.text_type(user.pk) + six.text_type(timestamp) +
-            six.text_type(user.profile.email_confirmed)
->>>>>>> f64f7a8d0b9819a4a2822b323c99e833a7085ee2
         )
 
 account_activation_token = AccountActivationTokenGenerator()
