d05683d224744a34fdc27b4715e850b471784ad9
alstjr0307 <alswp07@naver.com>
2021-02-25 12:02:09 +0900

    집컴 2.25

--- mysite/forms.py
+++ mysite/forms.py
@@ -3,20 +3,45 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class UserCreationForm(UserCreationForm):
-    email = forms.EmailField(label='이메일',required=True)
-    nickname = forms.CharField(label= '닉네임',max_length=12, min_length=2, required=True, help_text='Required: Nickname',
-                               widget=(forms.TextInput(attrs={'class': 'form-control'})))
+    
 
-
+    email = forms.EmailField(label='이메일',required=True, widget=(forms.TextInput(attrs={'class': 'form-control'})))
+    nickname = forms.CharField(label= '닉네임',max_length=6, min_length=2, required=True,
+                               widget=(forms.TextInput(attrs={'class': 'form-control', 'placeholder':'2자~6자까지 가능합니다.'})))
+    
     class Meta:
         model = User
-        fields = ("username", "nickname", "email", "password1", "password2",)
+        fields = ("username",  "email", "password1", "password2","nickname")
+        labels = {
+        'username': '아이디',
+        'email': '이메일',
+        'password1': '패스워드',
+        'password2': '패스워드 확인',
+        }
+        widgets = {
+        'username': forms.TextInput(attrs={'class': 'form-control', 'placeholder':'15자 이내로 입력 가능합니다.'}),
+        'password1' : forms.PasswordInput(attrs={'class': 'form-control'}),
+        'password2' : forms.PasswordInput(attrs={'class': 'form-control'}),
+        }
+
 
     def save(self, commit=True):
         user = super(UserCreationForm, self).save(commit=False)
-        user.email = self.cleaned_data["email"]
         user.first_name=self.cleaned_data["nickname"]
        
         if commit:
             user.save()
         return user
+
+class ProfileForm(forms.ModelForm):
+    first_name= forms.CharField(label='닉네임',max_length=12, min_length=2, required=True, help_text='Required: Nickname',
+                               widget=(forms.TextInput(attrs={'class': 'form-control'})))
+    
+    
+    class Meta:
+        model = User
+        fields = [
+            'username',
+            'first_name',
+            'email',
+            ]
--- mysite/views.py
+++ mysite/views.py
@@ -1,14 +1,24 @@
 from django.views.generic import TemplateView
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
 
-from django.views.generic import ListView
+from django.views.generic import ListView, View
 from django.contrib.auth.mixins import AccessMixin
 from blog.models import Post
 
-from .forms import UserCreationForm
+from .forms import UserCreationForm, ProfileForm
 from domestic.models import Post_Domestic
 from django.urls import reverse_lazy
+from django.contrib.auth.models import User
 
+from django.contrib import messages
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.template.loader import render_to_string
+from mysite.tokens import account_activation_token
+from django.shortcuts import render, redirect
+from django.utils.encoding import force_text
+from django.contrib.auth import login
 
 class HomeView(ListView):
     template_name='home.html'
@@ -23,12 +33,41 @@ class HomeView(ListView):
         return context
 
 
-
+##회원가입VIEW
 class SignUp(CreateView):
+    ##form은 forms.py에서 만든 UserCreationForm변형을 이용
     form_class = UserCreationForm
-    success_url = reverse_lazy('register_done')
+    ##template은 register.html
     template_name = 'registration/register.html'
+    
+    def get(self, request, *args, **kwargs):
+        form = self.form_class()
+        return render(request, self.template_name, {'form': form})
 
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+
+            user = form.save(commit=False)
+            
+            user.is_active = False # Deactivate account till it is confirmed
+            user.save()
+
+            current_site = get_current_site(request)
+            subject = 'Activate Your MySite Account'
+            message = render_to_string('registration/account_activation_email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': account_activation_token.make_token(user),
+            })
+            user.email_user(subject, message)
+
+            messages.success(request, ('Please Confirm your email to complete registration.'))
+
+            return redirect('login')
+
+        return render(request, self.template_name, {'form': form})
 class UserCreateDoneTV(TemplateView):
     template_name = 'registration/register_done.html' 
 
@@ -42,3 +81,29 @@ class OwnerOnlyMixin(AccessMixin):
             return self.handle_no_permission()
 
         return super().dispatch(request, *args,**kwargs)
+    
+class ProfileView(UpdateView):
+    model = User
+    form_class = ProfileForm
+    success_url = reverse_lazy('home')
+    template_name='registration/profile.html'
+
+class ActivateAccount(View):
+
+    def get(self, request, uidb64, token, *args, **kwargs):
+        try:
+            uid = force_text(urlsafe_base64_decode(uidb64))
+            user = User.objects.get(pk=uid)
+        except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+            user = None
+
+        if user is not None and account_activation_token.check_token(user, token):
+            user.is_active = True
+            
+            user.save()
+            login(request, user)
+            messages.success(request, ('Your account have been confirmed.'))
+            return redirect('home')
+        else:
+            messages.warning(request, ('The confirmation link was invalid, possibly because it has already been used.'))
+            return redirect('home')
