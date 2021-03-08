3f179a6bc7b4ae28e6a3005779b2f622c7509f5d
alstjr0307 <alswp07@naver.com>
2021-03-08 16:56:20 +0900

    ss

--- mysite/settings.py
+++ mysite/settings.py
@@ -53,7 +53,7 @@ MESSAGE_TAGS = {
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =  True
 
-ALLOWED_HOSTS = [ '15.164.164.21', 'ec2-15-164-164-21.ap-northeast-2.compute.amazonaws.com', '127.0.0.1']
+ALLOWED_HOSTS = [ '15.164.164.21', 'www.stockstorage.net']
 
 
 
